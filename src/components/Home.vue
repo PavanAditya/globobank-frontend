1fe4eab9914712e8f011bcf21aaa8fd3ef938b26
Pavanaditya Mylavarapu <M1050962@mindtree.com>
2019-12-07 18:25:57 +0530

    Components added with basic stub codes

---
+++ src/components/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2>Home</h2>
+    <transactions></transactions>
+    <edit-transactions></edit-transactions>
+  </div>
+</template>
+
+<script>
+import Transactions from './Transactions'
+import EditTransactions from './EditTransactions'
+  export default {
+    components: {
+      Transactions,
+      EditTransactions
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
